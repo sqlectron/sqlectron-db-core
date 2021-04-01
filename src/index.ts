b4b3393f7f5617c715f4bfedf5f67044de252e0b
Max Claus Nunes <maxcnunes@gmail.com>
2021-03-31 21:32:06 -0300

    Export types and methods used by sqlectron-gui

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+export { setSelectLimit, clearSelectLimit } from './database';
 export { ADAPTERS, ADAPTERS as CLIENTS } from './adapters';
 export { createServer } from './server';
+export { setLogger } from './logger';
+
+// Export types
+export type { Database } from './database';
+export type { Adapter } from './adapters';
+export type { QueryRowResult } from './adapters/abstract_adapter';
+export type { DatabaseFilter, SchemaFilter } from './filters';
+export type { Server, LegacyServerConfig } from './server';
